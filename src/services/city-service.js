129f3815871dd6696204a9ef3c84ae07e000f662
unknown <abhisiktj@gmail.com>
2023-10-11 15:45:13 +0530

    FC

---
+++ src/services/city-service.js
@@ -0,0 +1,54 @@
+const {CityRepository}=require('../repository/index');
+
+class CityService{
+    constructor(){
+        this.cityRepository=new CityRepository();
+    }
+
+    async createCity(name){
+        try{
+        const city=this.cityRepository.createCity({name});
+        return city;
+        }
+        catch(error){
+             throw error;
+        }
+
+    }
+    
+    async deleteCity(cityId){
+        try{
+        this.cityRepository.createCity({cityId});
+        return true;
+        }
+        catch(error){
+             throw error;
+        }
+
+    }
+
+    async updateCity(data,cityId){
+        try{
+        const city=this.cityRepository.createCity({cityId,data});
+        return city;
+        }
+        catch(error){
+             throw error;
+        }
+
+    }
+
+
+    async getCity(cityId){
+        try{
+        const city=this.cityRepository.createCity(cityId);
+        return city;
+        }
+        catch(error){
+             throw error;
+        }
+
+    }
+}
+
+module.exports=CityService;
